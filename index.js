a0549b7a2166b7415635acdbd8562102256ec391
davidfn268 <davidfn268@gmail.com>
2024-08-29 20:49:41 -0300

    commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const h5 = document.querySelector("h5");
+const text = document.querySelector(".card-text");
+const img = document.querySelector(".card img");
+
+h5.innerText = "Dibu";
+
+text.innerText = "el mejo del mundo";
+
+img.src = "https://fotografiamejorparavendermas.com/wp-content/uploads/2017/06/La-importancia-de-la-imagen.jpg";
